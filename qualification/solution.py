4c224b5b5cd89634b59b4724695e2935204db628
Artur Sokol <psyhoge@gmail.com>
2021-02-25 19:59:56 +0100

    add a comment with how to read the input file

--- qualification/solution.py
+++ qualification/solution.py
@@ -2,3 +2,26 @@ with open("a.txt") as input_data:
     simulationTime, intersectionsCount, streetsCount, carsCount, bonus = \
         list(map(int, input_data.readline().split()))
 
+
+
+"""
+from collections import defaultdict
+
+crossroads = defaultdict(defaultdict(list))
+streets_dict = {}
+
+for _ in range(streets):
+    start, end, name, weight = input_data.readline()
+    crossroads[int(end)]['in'].append(name)
+    crossroads[int(start)]['out'].append(name)
+    streets_dict[name] = {'start': int(start), 'end': int(end), 'weight': int(weight), 'cars': []}
+
+cars_list = []
+for i in range(cars):
+    items = input_data.readline().split(' ')
+    street_count = int(items[0])
+    route = items[1:]
+    start_street = route[0]
+    streets_dict[start_street]['cars'].append(i)
+    cars_list.append({'street_count': street_count, 'route': route})
+"""
